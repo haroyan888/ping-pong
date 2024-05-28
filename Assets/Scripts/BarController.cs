b070ee178c1404f1901b0e2322b48cd634c6e020
harusann <gp22a074@oecu.jp>
2024-05-29 07:24:46 +0900

    画面サイズの変更と位置の指定の変更

--- Assets/Scripts/BarController.cs
+++ Assets/Scripts/BarController.cs
@@ -7,25 +7,27 @@ using Unity.VisualScripting;
 public class BarController : MonoBehaviour
 {
     private Dictionary<string, Vector2> movableRange;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         movableRange = new Dictionary<string, Vector2>();
-        movableRange["min"] =  Camera.main.ViewportToWorldPoint(Vector2.zero);
-        movableRange["max"] =  Camera.main.ViewportToWorldPoint(Vector2.one);
+        movableRange["min"] = Camera.main.ViewportToWorldPoint(Vector2.zero);
+        movableRange["max"] = Camera.main.ViewportToWorldPoint(Vector2.one);
     }
 
     // Update is called once per frame
     void Update()
     {
         // 前に移動
-        if (Input.GetKey (KeyCode.UpArrow) && this.movableRange["max"].y >= this.transform.position.y) {
-            this.transform.Translate (0.0f,0.1f,0.0f);
+        if (Input.GetKey(KeyCode.UpArrow) && this.movableRange["max"].y >= this.transform.position.y)
+        {
+            this.transform.Translate(0.0f, 0.1f, 0.0f);
         }
         // 後ろに移動
-        if (Input.GetKey (KeyCode.DownArrow) && this.movableRange["min"].y <= this.transform.position.y) {
-             this.transform.Translate (0.0f,-0.1f,0.0f);
+        if (Input.GetKey(KeyCode.DownArrow) && this.movableRange["min"].y <= this.transform.position.y)
+        {
+            this.transform.Translate(0.0f, -0.1f, 0.0f);
         }
     }
 }
